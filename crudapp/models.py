23e33484b2f628bf217767a71ce1d5afa3d483ee
csathishkumar <csathishkumar2002@gmail.com>
2024-10-05 22:44:46 +0530

    Created a Index.html with Bootstrap

--- crudapp/models.py
+++ crudapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30,null=False,blank=False,verbose_name="First Name")
+    last_name = models.CharField(max_length=30,null=False,blank=False,verbose_name="Last name")
+    phone = models.CharField(max_length=10,null=False,blank=False,verbose_name="Phone Number")
+    email = models.EmailField(max_length=277,null=False,blank=False,verbose_name="Email Address")
+    address = models.TextField(max_length=100,verbose_name="Address")    
+    def __str__(self):
+        return str(self.id)
+    
+
