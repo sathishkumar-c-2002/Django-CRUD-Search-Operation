bc1d2c91ee34ea09e9b1c777417d911d62009587
csathishkumar <csathishkumar2002@gmail.com>
2024-10-06 18:37:17 +0530

    Created the function to add

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 from .models import Student
+from django.contrib import messages
 # Create your views here.
 def index(request):
     students = Student.objects.all()
+    if request.method == "POST":
+        if "add" in request.POST:
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            phone = request.POST.get("phone")
+            email = request.POST.get("email")
+            address = request.POST.get("address")
+            Student.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                phone=phone,
+                email=email,
+                address=address
+                )
+            messages.success(request,"Student Added Successfully")
+        
+        elif request.method == "POST":
+            if "add" in request.POST:
+                first_name = request.POST.get("first_name")
+                last_name = request.POST.get("last_name")
+                phone = request.POST.get("phone")
+                email = request.POST.get("email")
+                address = request.POST.get("address")
+                Student.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    phone=phone,
+                    email=email,
+                    address=address
+                    )
+                messages.success(request,"Student Added Successfully")
+                
+            
     return render(request,"index.html",{"students": students})
