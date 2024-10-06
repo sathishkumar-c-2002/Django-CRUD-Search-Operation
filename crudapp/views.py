e5115518e722b7d4c3c667d2367cf20d09fba25f
csathishkumar <csathishkumar2002@gmail.com>
2024-10-06 18:40:00 +0530

    Created the fuction to update

--- crudapp/views.py
+++ crudapp/views.py
@@ -20,21 +20,22 @@ def index(request):
                 )
             messages.success(request,"Student Added Successfully")
         
-        elif request.method == "POST":
-            if "add" in request.POST:
-                first_name = request.POST.get("first_name")
-                last_name = request.POST.get("last_name")
-                phone = request.POST.get("phone")
-                email = request.POST.get("email")
-                address = request.POST.get("address")
-                Student.objects.create(
-                    first_name=first_name,
-                    last_name=last_name,
-                    phone=phone,
-                    email=email,
-                    address=address
-                    )
-                messages.success(request,"Student Added Successfully")
+        elif "update" in request.POST:
+            id = request.POST.get("id")
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            phone = request.POST.get("phone")
+            email = request.POST.get("email")
+            address = request.POST.get("address")
+            
+            update_student = Student.objects.get(id=id)
+            update_student.first_name = first_name
+            update_student.last_name = last_name
+            update_student.phone = phone
+            update_student.email = email
+            update_student.address = address
+            update_student.save()
+            messages.success(request,"Student Updated Successfully")
                 
             
     return render(request,"index.html",{"students": students})
